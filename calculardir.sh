3fc879c4b970a927415ced1935fd6031519055c6
narellakloster <54905639+narellakloster@users.noreply.github.com>
2019-09-04 14:39:29 -0300

    Update calculardir.sh

--- calculardir.sh
+++ calculardir.sh
@@ -9,7 +9,14 @@ done
 
 if [ -r $directorio ]
 then
-echo "Total directorio $directorio = $(du -hs $directorio | cut -f1)"
+  total=0
+  for i in $directorio
+    do
+    echo $i
+    aux=$(stat -c %s $i)
+    total=$((total + $aux))
+    done
+  echo "Total directorio $directorio = $total"
 else
 echo "Error: No tienes permisos para leer este directorio"
 fi
