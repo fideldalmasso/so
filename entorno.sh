cf8b532557fa9c371e9c722b1fc2f2d84ae04986
juanpablocabana <juanpablocaban98@outlook.com>
2019-09-05 13:37:04 -0400

    Agrega puntos.

--- entorno.sh
+++ entorno.sh
@@ -3,11 +3,11 @@ echo "Bienvenido usuario: $USERNAME"
 
 echo "Usted se ha conectado en el día $(date +%d/%m/%Y)"
 
-echo "A la hora $(date +%Hhs:%Mm:%Ss)"
+echo "A la hora: $(date +%Hhs:%Mm:%Ss)"
 
-echo "En la terminal: $TERM del host $HOSTNAME"
+echo "En la terminal: $TERM del host: $HOSTNAME"
 
-echo "Se encuentran conectados en este momento, los siguientes usuarios"
+echo "Se encuentran conectados en este momento, los siguientes usuarios:"
 echo -e "\r$(who)"
 
 echo "Su path es:"
