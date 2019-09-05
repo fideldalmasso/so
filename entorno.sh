786d61df8ce11fd653e488866c026cf5806b8941
narellakloster <narellakloster79@gmail.com>
2019-09-05 12:36:49 -0400

    Corregido ej2 y completado ej3

---
+++ entorno.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Bienvenido usuario: $USERNAME"
+
+echo "Usted se ha conectado en el día $(date +%d/%m/%Y)"
+
+echo "A la hora $(date +%Hhs:%Mm:%Ss)"
+
+echo "En la terminal: $TERM del host $HOSTNAME"
+
+echo "Se encuentran conectados en este momento, los siguientes usuarios"
+echo -e "\r$(who)"
+
+echo "Su path es:"
+echo $PATH
+
+export PS1="\D{%Y}-\H:\w#"
