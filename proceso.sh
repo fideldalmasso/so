4079fd38293a11be9ff2efe292045855f5557c54
fideldalmasso <fidelmasso@gmail.com>
2019-09-06 22:50:13 -0300

    Resuelve ejercicio 5

---
+++ proceso.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ps
+find / -type f -name "c*" &> $1 &
+echo "Se está generando una lista de archivos que comienzan con c"
+ps
+read -p 'Ingrese el id del proceso de búsqueda de archivos que inician con letra "c":' id
+
+while [ $! != $id ]
+	do	
+	read -p "El id ingresado no es el solicitado, ingrese uno correcto:" id
+done
+
+#if kill -0 $!
+#if pgrep -x "find" > /dev/null
+if ps -p $id > /dev/null
+	then
+	kill "$id"
+	echo "Usted finalizó el proceso con éxito"
+	else
+	echo "Usted ingresó el id correcto, pero el proceso finalizó antes"
+fi
