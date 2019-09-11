e0a82ff6bd9bdc8ef4542818a54bb6fb0f78b34e
Juan Pablo Cabaña <46539158+JU4N98@users.noreply.github.com>
2019-09-11 14:47:57 -0300

    Cambia if-else por switch.

--- calcular.sh
+++ calcular.sh
@@ -6,32 +6,27 @@ num2=0
 read -p "Ingrese el nro1 --> " num1
 read -p "Ingrese el nro2 -> " num2
 
-if [ $operacion == "suma" ] 
-then
+case $operacion in
+	suma)
 	resultado=$(( $num1 + $num2 ))
 	echo "$num1 + $num2 = $resultado"
-else
-	if [ $operacion == "resta" ] 
-	then 
-		resultado=$(( $num1 - $num2 ))
-		echo "$num1 - $num2 = $resultado"
-	else
-		if [ $operacion == "multiplicacion" ] 
-		then 
-			resultado=$(( $num1 * $num2 )) 
-			echo "$num1 * $num2 = $resultado"
+	;;
+	resta)
+	resultado=$(( $num1 - $num2 ))
+	echo "$num1 - $num2 = $resultado"
+	;;
+	multiplicacion)
+	resultado=$(( $num1 * $num2 )) 
+	echo "$num1 * $num2 = $resultado"
+	;;
+	division)
+		if [ $num2 == 0 ]
+		then
+			echo "La division por 0 no esta definida"
 		else
-			if [ $operacion == "division" ]
-			then
-				if [ $num2 == 0 ]
-				then
-					echo "La division por 0 no esta definida"
-				else
-					resultado=$(( $num1 / $num2 ))
-					echo "$num1 / $num2 = $resultado"
-				fi
-			fi
-			
+			resultado=$(( $num1 / $num2 ))
+			echo "$num1 / $num2 = $resultado"
 		fi
-	fi
-fi
+	;;
+	*)
+esac
