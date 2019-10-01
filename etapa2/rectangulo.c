7eccb46f7583cf8a5fb77c93f1a291b59ffb2bcc
fideldalmasso <fidelmasso@gmail.com>
2019-10-01 10:16:49 -0300

    actualiza gitignore. continua con ejercicio1

--- etapa2/rectangulo.c
+++ etapa2/rectangulo.c
@@ -3,23 +3,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <getopt.h>
 
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
-  int perimetro=false;
-  int area=false;
+  bool mostrarArea=false;
+  bool mostrarPerimetro=false;
   int c;
+  int area=0;
+  int perimetro=0;
+  int lado=-1;
+  int base=-1;
 
 
-  while ((c = getopt (argc, argv, "ap")) != -1)
+  while ((c = getopt (argc, argv, "apl:b:")) != -1)
     switch (c)
       {
       case 'a':
-        area = true;
+        mostrarArea = true;
         break;
       case 'p':
-        perimetro = true;
+        mostrarPerimetro = true;
+        break;
+      case 'l':
+        lado=atoi(optarg);
+ 
+        break;
+      case 'b':
+        base=atoi(optarg);
+
         break;
       /*case '?':
         if (isprint (optopt))
@@ -34,8 +46,16 @@ main (int argc, char **argv)
         abort ();
       }
 
+ if (lado <= 0 ) {
+       printf("-l es obligatorio!\n");
+       exit(1);
+    }
+ if (base <= 0 ) {
+       printf("-b es obligatorio!\n");
+       exit(1);
+    }
 
-  printf ("area = %d, perimetro = %d\n", area,perimetro);
+  printf ("mostrarArea = %d, mostrarPerimetro = %d, lado = %d, base = %d\n", mostrarArea, mostrarPerimetro, lado, base);
 
   return 0;
 }
