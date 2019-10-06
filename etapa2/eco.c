7d08a7cf03c9a581b7d5c1569e4585d09ab08846
juanpablocabana <juanpablocaban98@outlook.com>
2019-10-06 18:21:03 -0400

    Agrega eco.c

---
+++ etapa2/eco.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main (int argc, char *argv[]) {
+	int i;
+	printf("voy a gritar!!!!: %d\n", getpid());
+	for(i=1; i<4; i++){
+		fork();
+		printf("holaaaaa....)))))))) %d reflejo: %d sonido: %d \n", i, getpid(), getppid());
+		
+	}
+	
+	return 0;
+}
