c70159c179685f96e734fe73c5ba23584c31b83a
Fidel Dalmasso <fideldalmasso@gmail.com>
2019-10-21 13:24:39 -0400

    Termina de arreglar ej1 y ej2. Faltan retoques

--- etapa4/buddyFS2.c
+++ etapa4/buddyFS2.c
@@ -19,6 +19,12 @@
 #define EXT2_S_IFDIR    0x4000  /* directory */
 
 
+#define printf1(...) printf("%-40s%12s\n", __VA_ARGS__)
+#define printf2(...) printf("%-40s%12d\n", __VA_ARGS__)
+#define printf4(x) printf("%s\n",x)
+#define printf5(x) printf("%d\n",x)
+
+
 void fechaF(int segundos, char fechaS []);
 void grupoF(int gid, char grupoS []);
 void imprimir1(char texto1[], char texto2[]);
@@ -26,7 +32,6 @@ void imprimir2(char texto1[], int * numero);
 void imprimirBits (int num);
 void leer1(int fd, int offset, int bytes, char buffer[]);
 void leer2(int fd, int offset, int bytes, int *buffer);
-//void leer3(int fd, int offset, int bytes, struct *buffer);
 void leerEImprimir1(int fd, int offset, int bytes, char buffer[], char texto[]);
 void leerEImprimir2(int fd, int offset, int bytes, int *buffer, char texto[]);
 void mostrarError(char texto[]);
@@ -56,10 +61,7 @@ void leer2(int fd, int offset, int bytes, int *buffer) {
     if (lseek(fd, offset, SEEK_SET) == -1) mostrarError("lseek");
     if (read(fd, buffer, bytes) == -1) mostrarError("read");
 }
-/*void leer3(int fd, int offset, int bytes, struct * buffer){
-    if (lseek(fd, offset, SEEK_SET) == -1) mostrarError("lseek");
-    if (read(fd, buffer, bytes) == -1) mostrarError("read");
-}*/
+
 void leerEImprimir1(int fd, int offset, int bytes, char buffer[], char texto[]) {
     leer1(fd, offset, bytes, buffer);
     imprimir1(texto, buffer);
@@ -85,21 +87,13 @@ void imprimirBits (int num) {
 }
 
 
-void mostrarOpciones(){
-
-    printf("Modo de uso\nbuddyFS -s\nbuddyFS -l\nbuddyFS -b tamanioNube\n");
-    exit(0);
-}
-
-
 //ejercicio1------------------------------------------------------------------------
 
 void ejercicio1(struct ext2_super_block sb) {
 
-    if (sb.s_magic != EXT2_SUPER_MAGIC) {
-        printf("%s\n", "No hay sistema de archivos tipo EXT2");
-        exit(1);
-    }
+
+    printf("-----------------INFORMACION DEL SB-----------------\n");
+
     printf("%-40s%12s\n", "Sistema de archivos tipo:","EXT2");
     printf("%-40s%12s\n", "Nombre del volumen:",sb.s_volume_name);
     printf("%-40s%12d\n", "Cantidad de i-nodos:",sb.s_inodes_count);
@@ -111,7 +105,7 @@ void ejercicio1(struct ext2_super_block sb) {
     printf("%-40s%12d\n", "Cantidad de bloques:",sb.s_blocks_count);
     printf("%-40s%12d\n", "Cantidad de bloques libres:",sb.s_free_blocks_count);
     printf("%-40s%12d\n", "Tamanio total en disco:",sb.s_blocks_count * (sb.s_log_block_size+1024));
-    
+    printf("----------------------------------------------------\n");
 
 }
 
@@ -161,101 +155,39 @@ void obtenerModo(int modo, char salida[11]) {
     strcpy(salida, salida2);
 
 }
-/*
-Archivo cargarArchivo(int entradaDirectorio, FS s) {
 
-    Archivo i;
-    i.entradaDirectorio = entradaDirectorio;
+void ejercicio2(int fd1, struct ext2_super_block sb, struct ext2_group_desc gd, struct ext2_inode ti[]) {
 
-    i.inodo = i.recLen = i.modo = i.cantLinks = 0;
-    i.usuario = i.grupo =  i.tamanio = i.fecha = i.nameLen = 0;
-    char cadenaVacia[255] = {' '}; 
-    strncpy(i.nombreTexto,cadenaVacia,255);
-
-    leer2(s.fd, i.entradaDirectorio, 4, &i.idInodo);
-
-    leer2(s.fd, i.entradaDirectorio + 4, 2, &i.recLen);
-    i.inodo = s.tablaInodos + ((i.idInodo - 1) * s.tamanioInodoEnBytes);
-    leer2(s.fd, i.inodo, 2, &i.modo);
-    obtenerModo(i.modo, i.modoTexto);
-
-    leer2(s.fd, i.inodo + 26, 2, &i.cantLinks);
-    leer2(s.fd, i.inodo + 4, 4, &i.tamanio);
-
-    leer2(s.fd, i.inodo + 16, 4, &i.fecha);
-    fechaF(i.fecha, i.fechaTexto);
-
-    leer2(s.fd, i.inodo + 24, 2, &i.grupo);
-    grupoF(i.grupo, i.grupoTexto);
-
-    leer2(s.fd, i.inodo + 2, 2, &i.usuario);
-    usuarioF(i.usuario, i.usuarioTexto);
-
-    leer2(s.fd, i.entradaDirectorio + 6, 1, &i.nameLen);
-    leer1(s.fd, i.entradaDirectorio + 8, i.nameLen, i.nombreTexto);
-    return i;
-
-};
-*/
-void ejercicio2(int fd1, struct ext2_super_block sb, struct ext2_group_desc gd, struct ext2_inode i) {
-
-	int tamInodo= sb.s_inode_size;
 	int tamBloque= sb.s_log_block_size + 1024; 
 	
-	
-	
-	int directorioRaiz = (gd.bg_inode_table * tamBloque)+ tamInodo;
-	
-	//struct ext2_inode i;
-	//struct ext2_inode * i=malloc(sizeof(struct ext2_inode));
-
-	//printf("dirraiz %d\n",directorioRaiz);
-	//printf("bginodetalbe %d\n",gd.bg_inode_table);
-	//printf("tambloque %d\n",tamBloque);
-	if (lseek(fd1, directorioRaiz, SEEK_SET) == -1) mostrarError("lseek");
-    if (read(fd1, &i, tamInodo) == -1) mostrarError("read");
+     __le32 idBloqueDeEntradasRaiz = ti[1].i_block[0];
+    int entradaDirectorio = idBloqueDeEntradasRaiz * tamBloque;
+    printf("entradadir%d\n",entradaDirectorio);
     
-     //__le32 bloqueEntradasRaiz;
-     __le32 idBloqueDeEntradasRaiz = i.i_block[0];
-    // printf("idber:%d\n", idBloqueDeEntradasRaiz);
-     int entradaDirectorio = idBloqueDeEntradasRaiz * tamBloque;
-    // printf("entradadir%d\n",entradaDirectorio);
-    
-    //int primerBloqueEntradaRaiz; 
-    //if (lseek(fd1, directorioRaiz, SEEK_SET) == -1) mostrarError("lseek");
-    //if (read(fd1, i, tamInodo) == -1) mostrarError("read");
-    
-    //printf("%d",i->i_block);
-    
-	
-    //int entradaDirectorio = s.primerEntradaDirectorio;
-    int aux=0, recLen=0, nameLen=0, dirInodo=0;
     printf("%-8s %-16s %-6s %-8s %-8s %-8s %-16s %-16s\n", "Inodo", "Modo", "Links", "Usr", "Grp", "Tamanio", "Fecha", "Archivos");
     while (1) {
+		int inodo =0, recLen =0, nameLen =0, filetype = 0;
+		char nombreTexto[255] = {' '};
+		char usuarioTexto[16] = {' '};
+		char grupoTexto[16] = {' '};
+		char fechaTexto[24] = {' '};
+		char modoTexto[11] = {' '};
 		
-		if (lseek(fd1, entradaDirectorio + 4, SEEK_SET) == -1) mostrarError("lseek");
+		if (lseek(fd1, entradaDirectorio, SEEK_SET) == -1) mostrarError("lseek");
+		if (read(fd1, &inodo, 4) == -1) mostrarError("read");
 		if (read(fd1, &recLen, 2) == -1) mostrarError("read");
-		
-		if (lseek(fd1, entradaDirectorio + 6, SEEK_SET) == -1) mostrarError("lseek");
 		if (read(fd1, &nameLen, 1) == -1) mostrarError("read");
+		if (read(fd1, &filetype, 1) == -1) mostrarError("read");
+		if (read(fd1, nombreTexto, nameLen) == -1) mostrarError("read");
 		
-       // Archivo i = cargarArchivo(entradaDirectorio, s);
+        obtenerModo(ti[inodo-1].i_mode,modoTexto);
+        fechaF(ti[inodo-1].i_mtime, fechaTexto);
+		grupoF(ti[inodo-1].i_gid, grupoTexto);
+		usuarioF(ti[inodo-1].i_uid, usuarioTexto);
+
+        printf("%-8d %-16s %-6d %-8s %-8s %-8d %-16s %-16s\n", inodo, modoTexto, ti[inodo-1].i_links_count, usuarioTexto, grupoTexto, ti[inodo-1].i_size, fechaTexto, nombreTexto);
         if ((entradaDirectorio + recLen) % tamBloque == 0) break;
-        
-        struct ext2_dir_entry * di = malloc(8+nameLen);
-        
-        if (lseek(fd1, entradaDirectorio, SEEK_SET) == -1) mostrarError("lseek");
-		if (read(fd1, &di, 8+nameLen) == -1) mostrarError("read");
-        
-        
-        //dirInodo = (di->inode -1) ;
-        if (lseek(fd1, di->inode, SEEK_SET) == -1) mostrarError("lseek");
-		if (read(fd1, &i, tamInodo) == -1) mostrarError("read");
-        
-        
-//printf("%-8d %-16s %-6d %-8s %-8s %-8d %-16s %-16s\n", i.idInodo, i.modoTexto, i.cantLinks, i.usuarioTexto, i.grupoTexto, i.tamanio, i.fechaTexto, i.nombreTexto);
-       // entradaDirectorio += i.recLen;
-        aux++;
+        entradaDirectorio +=recLen;
     }
 
 }
@@ -264,42 +196,61 @@ void ejercicio2(int fd1, struct ext2_super_block sb, struct ext2_group_desc gd, 
 
 
 int main(int argc, char * argv[]) {
-
+	if(argc<3){
+		printf("Modo de uso\nbuddyFS [-s][-l][-b tamanioNube] /rutaimagen\n");
+		exit(0);
+		};
+	
     int fd1;
-    if ((fd1 = open("extra/imagen1.flp", O_RDONLY) ) == -1) mostrarError("open");
+    if ((fd1 = open(argv[2], O_RDONLY) ) == -1) mostrarError("open");
 
-    struct ext2_super_block * sb =malloc(sizeof(struct ext2_super_block));
-    struct ext2_group_desc * gd =malloc(sizeof(struct ext2_group_desc));
-
-    //if (sb == NULL) {printf("errorEnMalloc\n"); exit(1);}
-
-
+    struct ext2_super_block * sb =malloc(1024);
+	
 	if (lseek(fd1, 1024, SEEK_SET) == -1) mostrarError("lseek");
     if (read(fd1, sb, 1024) == -1) mostrarError("read");
+    
+    if (sb->s_magic != EXT2_SUPER_MAGIC) {
+        printf("%s\n", "No hay sistema de archivos tipo EXT2");
+        exit(1);
+    }
+     struct ext2_group_desc * gd =malloc(1024);
+	struct ext2_inode * i=(struct ext2_inode*)malloc(128);
     
     if (lseek(fd1, 1024 * 2, SEEK_SET) == -1) mostrarError("lseek");
     if (read(fd1, gd, 1024) == -1) mostrarError("read");
 
-	int cantInodos = sb->s_inodes_count;
-	int tamInodo = sb->s_inode_size;
-	struct ext2_inode * ti=calloc(cantInodos,tamInodo);
+	__le32  cantInodos = (sb->s_inodes_count) - (sb->s_free_inodes_count);
 
+	struct ext2_inode ti[184];             //tabla de inodos
+
+	int posTablaInodos = (gd->bg_inode_table * 1024);
+	int p;
+	for(p=0;p<cantInodos;p++){
+		if (lseek(fd1, posTablaInodos + (p*128), SEEK_SET) == -1) mostrarError("lseek");
+		if (read(fd1, i, 128) == -1) mostrarError("read");
+		ti[p] = *i;
+
+}
 
     int c;
 
-    if(argc==1) mostrarOpciones();
 
-    if ((c = getopt (argc, argv, "sl")) < 0) mostrarError("getopt");
+
+    if ((c = getopt (argc, argv, "slb")) < 0) mostrarError("getopt");
 
     switch (c) {
     case 's':
        ejercicio1(*sb);
         break;
     case 'l':
-		ejercicio2(fd1,*sb,*gd, *ti);
+		ejercicio2(fd1,*sb,*gd, ti);
         break;
+    case 'b':
+		break;
     }
+    
     return 0;
+    
 }
 
 
